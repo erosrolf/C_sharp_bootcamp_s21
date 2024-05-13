06d2f10044a3d355714b6e602af91689c5cefedb
Eros Rolfe <erosrolf@ge-l6.kzn.21-school.ru>
2024-05-13 15:55:21 +0300

    added AddHabitView

---
+++ Team00/rush00/rush00.App/Views/AddHabbitView.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace rush00.App.Views
+{
+    public partial class AddHabbitView : UserControl
+    {
+        public AddHabbitView()
+        {
+            InitializeComponent();
+        }
+    }
+}
